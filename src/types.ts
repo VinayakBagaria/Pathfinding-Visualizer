d5f9ff2646edc87d8ac6c3b9de335634a7a194b0
VinayakBagaria <workvinayak95@gmail.com>
2023-07-01 15:07:30 +0530

    Ability to change speed during visualization

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,5 @@ export type NodeStatusType = 'start' | 'end' | 'unvisited' | 'wall' | 'visited';
 export type AlgorithmType = 'dfs' | 'bfs';
 
 export type SpeedType = 'fast' | 'average' | 'slow';
+
+export type AnimationType = 'travel' | 'shortest-path';
